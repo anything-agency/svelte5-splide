53f2b752d347b2197783dbf6b258da41660bc25d
Matthew Bromley <96053035+MattBromley98@users.noreply.github.com>
2025-05-21 11:33:52 +0100

    fix: Ensure svelte splide works with the latest dependency changes, include vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
-
   kit: {
-    adapter: adapter(),
-    prerender: {
-      default: true,
-    },
+    adapter: adapter({
+      // Enable prerendering by default through the adapter
+      prerender: {
+        default: true
+      }
+    })
   },
 };
 
